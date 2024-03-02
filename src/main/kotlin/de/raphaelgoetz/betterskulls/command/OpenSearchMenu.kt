4062df75ebf4da79fa1f8863315864dceb8dbe5c
Raphael Goetz <raphael.goetz.7@gmail.com>
2024-03-02 12:51:05 +0100

    feat: added search command for skulls

---
+++ src/main/kotlin/de/raphaelgoetz/betterskulls/command/OpenSearchMenu.kt
@@ -0,0 +1,24 @@
+package de.raphaelgoetz.betterskulls.command
+
+import de.raphaelgoetz.betterskulls.manager.SkullManager
+import de.raphaelgoetz.betterskulls.menu.SearchMenu
+import net.axay.kspigot.gui.openGUI
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class OpenSearchMenu(private val skullManager: SkullManager) : CommandExecutor {
+
+    override fun onCommand(p0: CommandSender, p1: Command, p2: String, p3: Array<out String>?): Boolean {
+        if (p0 !is Player) return true
+        if (p3 == null) return true
+        if (p3[0].isEmpty()) return true
+
+        val list = skullManager.search(p3[0])
+        val menu = SearchMenu(skullManager, list).searchMenu
+        p0.openGUI(menu)
+
+        return false
+    }
+}
---
+++ src/main/kotlin/de/raphaelgoetz/betterskulls/menu/SearchMenu.kt
@@ -0,0 +1,56 @@
+package de.raphaelgoetz.betterskulls.menu
+
+import de.raphaelgoetz.betterskulls.data.SkullData
+import de.raphaelgoetz.betterskulls.manager.SkullManager
+import net.axay.kspigot.chat.KColors
+import net.axay.kspigot.chat.literalText
+import net.axay.kspigot.gui.*
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class SearchMenu(
+    private val skullManager: SkullManager,
+    private val skulls: List<SkullData>
+) {
+
+    val searchMenu = kSpigotGUI(GUIType.SIX_BY_NINE) {
+
+        title = literalText("SkullMenu (${skulls.size})") {
+            color = KColors.GRAY
+        }
+
+        page(1) {
+            val searchedSkulls = createRectCompound<SkullData>(
+                Slots.RowTwoSlotOne, Slots.RowSixSlotNine,
+
+                iconGenerator = {
+                    it.head
+                },
+
+                onClick = { clickEvent, element ->
+                    val data = skullManager.skullData[element.name] ?: return@createRectCompound
+                    val gui = SkullSubMenu(data, element.name).skullSubMenu
+                    clickEvent.player.openGUI(gui)
+                }
+            )
+
+            searchedSkulls.addContent(skulls)
+
+            transitionFrom = PageChangeEffect.SWIPE_VERTICALLY
+            transitionTo = PageChangeEffect.SWIPE_VERTICALLY
+
+            compoundScroll(
+                Slots.RowOneSlotNine,
+                ItemStack(Material.ARROW), searchedSkulls, scrollTimes = 6
+            )
+            compoundScroll(
+                Slots.RowOneSlotOne,
+                ItemStack(Material.ARROW), searchedSkulls, scrollTimes = 6, reverse = true
+            )
+
+            onClose {
+                searchedSkulls.removeContent(skulls)
+            }
+        }
+    }
+}
